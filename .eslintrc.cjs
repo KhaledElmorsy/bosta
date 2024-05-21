cafeb483e26bd4b9c8259387e7137461453fbb46
KhaledElmorsy <khaled.mg.a@gmail.com>
2024-05-21 21:03:34 +0300

    fix: fix typo in eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePatters: '_' }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '_' }],
   },
 };
