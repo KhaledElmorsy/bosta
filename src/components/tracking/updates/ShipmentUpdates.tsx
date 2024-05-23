db2454ba21acf8445ddbb0aab0249c10c8debbba
KhaledElmorsy <khaled.mg.a@gmail.com>
2024-05-23 05:53:13 +0300

    fix: add table row in shipment details header

--- src/components/tracking/updates/ShipmentUpdates.tsx
+++ src/components/tracking/updates/ShipmentUpdates.tsx
@@ -12,39 +12,44 @@ export default function ShipmentUpdates() {
   }
 
   return (
-      <table>
-        <thead>
+    <table>
+      <thead>
+        <tr>
           <th>{t('common.branch')}</th>
           <th>{t('common.date')}</th>
           <th>{t('common.time')}</th>
           <th>{t('common.details')}</th>
-        </thead>
-        <tbody>
-          {fillEventHubs(shipment.TransitEvents).map((e) => {
-            return (
-              <tr>
-                <td>{e.hub ? t(`tracking.hub.${e.hub}`) : ''}</td>
-                <td>{e.timestamp.toLocaleDateString()}</td>
-                <td dir="ltr">
-                  {e.timestamp
-                    .toLocaleTimeString('en-US', {
-                      hour: 'numeric',
-                      minute: 'numeric',
-                    })
-                    .toLowerCase()}
-                </td>
-                <td>
-                  <p>{t(`tracking.state.${e.state}`)}</p>
-                  {e.reason && (
-                    <p className={style.reason} style={{ color: trackingColorMap[e.state] }}>
-                      {t(`tracking.reason.${e.reason}`)}
-                    </p>
-                  )}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+        </tr>
+      </thead>
+      <tbody>
+        {fillEventHubs(shipment.TransitEvents).map((e) => {
+          return (
+            <tr>
+              <td>{e.hub ? t(`tracking.hub.${e.hub}`) : ''}</td>
+              <td>{e.timestamp.toLocaleDateString()}</td>
+              <td dir="ltr">
+                {e.timestamp
+                  .toLocaleTimeString('en-US', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                  })
+                  .toLowerCase()}
+              </td>
+              <td>
+                <p>{t(`tracking.state.${e.state}`)}</p>
+                {e.reason && (
+                  <p
+                    className={style.reason}
+                    style={{ color: trackingColorMap[e.state] }}
+                  >
+                    {t(`tracking.reason.${e.reason}`)}
+                  </p>
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
